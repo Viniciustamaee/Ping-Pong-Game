ef839a0ba362c077a69292387779e5d8c74d8993
PEdbot <vinicius.tamae@pedbot.com.br>
2023-12-13 17:53:43 -0300

    fixed-Js-HTML-CSS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Chamando os botoes
+// Chamando os botões
 const adv1 = document.querySelector('#adv1');
 const adv2 = document.querySelector('#adv2');
 const reset = document.querySelector('#reset');
@@ -6,32 +6,41 @@ const placarAdv1 = document.querySelector('#adv1Placar');
 const placarAdv2 = document.querySelector('#adv2Placar');
 
 // Chamando a Select
-const game = document.querySelector('#games')
+const game = document.querySelector('#games');
 
 // Placar do jogo
 let scoreAdv1 = 0;
 let scoreAdv2 = 0;
 
 adv1.addEventListener('click', () => {
-    scoreAdv1 += 1
+    scoreAdv1 += 1;
     placarAdv1.innerText = scoreAdv1;
-    game.disabled = true
+    game.disabled = true;
     if (scoreAdv1 == game.value) {
+        const label = document.createElement('label');
+        label.textContent = 'Parabéns Primeiro Jogador';
+        document.body.appendChild(label);
         adv1.disabled = true;
         adv2.disabled = true;
-
+        placarAdv1.style.color = 'green';
+        placarAdv2.style.color = 'red';
     }
-})
+});
 
 adv2.addEventListener('click', () => {
-    scoreAdv2 += 1
-    game.disabled = true
-    placarAdv2.innerText = scoreAdv2
+    scoreAdv2 += 1;
+    game.disabled = true;
+    placarAdv2.innerText = scoreAdv2;
     if (scoreAdv2 == game.value) {
+        const label = document.createElement('label');
+        label.textContent = 'Parabéns Segundo Jogador';
+        document.body.appendChild(label);
         adv1.disabled = true;
         adv2.disabled = true;
+        placarAdv1.style.color = 'red';
+        placarAdv2.style.color = 'green';
     }
-})
+});
 
 reset.addEventListener('click', () => {
     scoreAdv1 = 0;
@@ -41,8 +50,6 @@ reset.addEventListener('click', () => {
     adv1.disabled = false;
     adv2.disabled = false;
     game.disabled = false;
-
-}
-)
-
-
+    placarAdv1.style.color = 'black';
+    placarAdv2.style.color = 'black';
+});
