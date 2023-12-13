c067597b04650005d145727a6a6097f4c023daef
PEdbot <vinicius.tamae@pedbot.com.br>
2023-12-13 16:29:57 -0300

    fixed-Js-Html

--- app.js
+++ app.js
@@ -12,19 +12,37 @@ const game = document.querySelector('#games')
 let scoreAdv1 = 0;
 let scoreAdv2 = 0;
 
-adv1.addEventListener('click',()=>{
+adv1.addEventListener('click', () => {
     scoreAdv1 += 1
     placarAdv1.innerText = scoreAdv1;
+    game.disabled = true
+    if (scoreAdv1 == game.value) {
+        adv1.disabled = true;
+        adv2.disabled = true;
+
+    }
 })
 
-adv2.addEventListener('click', ()=>{
+adv2.addEventListener('click', () => {
     scoreAdv2 += 1
+    game.disabled = true
     placarAdv2.innerText = scoreAdv2
+    if (scoreAdv2 == game.value) {
+        adv1.disabled = true;
+        adv2.disabled = true;
+    }
 })
 
-reset.addEventListener('click',()=>{
-    placarAdv1.innerText = 0
-    placarAdv2.innerText = 0
-})
+reset.addEventListener('click', () => {
+    scoreAdv1 = 0;
+    scoreAdv2 = 0;
+    placarAdv1.innerText = '0';
+    placarAdv2.innerText = '0';
+    adv1.disabled = false;
+    adv2.disabled = false;
+    game.disabled = false;
+
+}
+)
 
 
